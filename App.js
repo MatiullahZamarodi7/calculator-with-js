615e5390742242e21b15fad01d787e5178fe2213
MatiullahZamarodi <zamarodimatiullah@gmail.com>
2026-04-03 05:21:30 -0700

    all files add

--- App.js
+++ App.js
@@ -1,4 +1,55 @@
+let input = document.querySelector('.input');
 let numbers = document.querySelectorAll('.value');
+let AC = document.querySelector('.AC');
+let DEL = document.querySelector('.DEL');
+let equal = document.querySelector('.equal');
 
 
 
+numbers.forEach(item => {
+    item.addEventListener('click', () => {
+        input.value += item.value;
+    })
+});
+
+AC.addEventListener('click', () => {
+    if (input.value === "") {
+        Swal.fire({
+            title: 'خطا',
+            text: 'چیزی موجود نیست!',
+            icon: 'error'
+        });
+    }
+    input.value = "";
+})
+
+DEL.addEventListener('click', () => {
+    if (input.value === "") {
+        Swal.fire({
+            title: 'خطا',
+            text: 'چیزی موجود نیست!',
+            icon: 'error'
+        });
+    }
+    input.value = input.value.slice(0, -1);
+})
+
+equal.addEventListener('click', () => {
+    if (input.value === "") {
+        Swal.fire({
+            title: 'خطا',
+            text: 'چیزی موجود نیست!',
+            icon: 'error'
+        });
+    } else {
+        try {
+            input.value = eval(input.value);
+        } catch (e) {
+            Swal.fire({
+                title: 'خطا',
+                text: e.message,
+                icon: 'error'
+            });
+        }
+    }
+})
